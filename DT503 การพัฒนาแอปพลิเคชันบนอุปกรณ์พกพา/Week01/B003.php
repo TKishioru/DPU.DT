98bccefcccb7281c5051bf6a087abc5c3a6e7d35
KHAMPEERADA P. <54303135+TKishioru@users.noreply.github.com>
2025-09-07 10:00:03 +0700

    new file + finish ex_week 3 (no.1-2)

---
+++ DT503 การพัฒนาแอปพลิเคชันบนอุปกรณ์พกพา/Week01/B003.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <title>ตัวอย่างการใช้งาน PHP</title>
+</head>
+
+<body>
+    <h4>ตัวอย่างการใช้งาน PHP</h4>
+<?php
+echo "Hello World<br>";
+echo "สวัสดีชาวโลก<br>";
+$name = "Khampeerada Phhothong";
+echo "My name is $name";
+
+$x = "TK"; //ตัวแปรชนิด string
+echo "<br>My name is $x";
+$x = 10 - 5; //ตัวแปรชนิด int
+print "<br>10 - 5 = $x";
+$a = 10;
+echo $a.=4; //เป็น String 
+++$a; //Pre-increment
+$a++; //Pro-increment
+?>
+
+
+</body>
+
+</html>
